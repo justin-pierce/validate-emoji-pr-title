2ddc6600fc0c9ae980fe6aa63c8d8746f92e9c01
Justin Pierce <2170989+justin-pierce@users.noreply.github.com>
2025-01-18 16:21:45 -0500

    get it built with github actions

--- src/run.ts
+++ src/run.ts
@@ -12,7 +12,7 @@ export const run = (context: Context) => {
 
   const pullRequestTitle = context?.payload?.pull_request?.title;
 
-  info(`Pull Request: "${pullRequestTitle}"`);
+  info(`Pull Request ts: "${pullRequestTitle}"`);
 
   const regex = RegExp(getInput("regexp"), getInput("flags"));
   const helpMessage = getInput("helpMessage");
